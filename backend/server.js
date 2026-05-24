f65b7a6c99a1564b9ed4aff1cf20d0d86899dfbf
Abdul Rehman <theee.abdulrehman@gmail.com>
2026-05-24 12:59:29 +0500

    Fix: Railway dotenv issue

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-require('dotenv').config();
+try { require('dotenv').config(); } catch(e) {}
 
 const productRoutes = require('./routes/products');
 const authRoutes = require('./routes/auth');
